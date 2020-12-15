ea66c03fc128a79b33d3bbd6fdc5e5456ca65ebf
Niels Gorter <nierls@live.nl>
2020-12-15 20:20:49 +0100

    it worky

--- flip.c
+++ flip.c
@@ -17,10 +17,8 @@
 #include <errno.h> // for perror()
 #include <pthread.h>
 
-#include "uint128.h"
 #include "flip.h"
 #include "semaphore.h"
-#include <math.h>
 
 // Some basic defines taken from threaded_basics.c
 
@@ -37,15 +35,18 @@
 #define BIT_CLEAR(v, n) ((v) = (v) & ~BITMASK(n))
 
 sem_t sem;
+// int free_ids[NROF_THREADS];
 
 static void *flip_thread(void *arg);
-int min(int a, int b);
+// int min(int a, int b);
+// void free_id(int id);
+// int take_id();
 
-typedef struct
-{
-    int i;
-    pthread_t id;
-} THREAD_ARGS;
+// typedef struct
+// {
+//     int i;
+//     pthread_t id;
+// } THREAD_ARGS;
 
 int main(void)
 {
@@ -63,77 +64,110 @@ int main(void)
     int NROF_ELEMENTS = (NROF_PIECES / 128) + 1;
 
     for (int i = 0; i < NROF_ELEMENTS; i++)
-    {
         buffer[i] = ~0;
-    }
 
-    int current_id;
-    pthread_t thread_id[NROF_THREADS];
-    THREAD_ARGS parameters[NROF_THREADS];
-    //sem_init(&sem, 0, NROF_THREADS);
+    // for (int i = 0; i < NROF_THREADS; i++)
+    //     free_ids[i] = i;
+
+    // pthread_t threads[NROF_THREADS];
+    //THREAD_ARGS parameters;
+    sem_init(&sem, 0, NROF_THREADS);
 
     for (int i = 2; i < NROF_PIECES; i++)
     {
-        //sem_wait(&sem);
-        current_id = (i - 2) % NROF_THREADS;
-        parameters[current_id].id = current_id;
-        parameters[current_id].i = i;
+        sem_wait(&sem);
+        pthread_t thread;
+        //int id = take_id();
 
-        if (pthread_create(&thread_id[current_id], NULL, &flip_thread, (void *)&parameters) != 0)
-        {
+        // if (id != -1)
+        // {
+        int *parameter = malloc(sizeof(int));
+        *parameter = i;
+
+        if (pthread_create(&thread, NULL, flip_thread, parameter) != 0)
             perror("pthread_create() error");
-        }
+        // }
+        // else
+        // {
+        //     fprintf(stderr, "Thread did not exit before new thread was created");
+        // }
     }
 
-    for (int i = 0; i < min(NROF_THREADS, NROF_PIECES - 2); i++)
-    {
-        pthread_join(thread_id[i], NULL);
-    }
+    // for (int i = 0; i < min(NROF_THREADS, NROF_PIECES - 2); i++)
+    //     pthread_join(threads[i], NULL);
 
-    //sem_destroy(&sem);
+    for (int i = 0; i < NROF_THREADS; i++)
+        sem_wait(&sem);
 
-    for (int i = 0; i < NROF_PIECES; i++)
-    {
+    sem_destroy(&sem);
+
+    for (int i = 1; i < NROF_PIECES; i++)
         if (BIT_IS_SET(buffer[i / 128], i % 128))
-        {
-            //printf("%d\n", i);
-        }
-    }
+            fprintf(stdout, "%d\n", i);
 
     return (0);
 }
 
 static void *flip_thread(void *arg)
 {
-    // TODOperror("test1");
-    THREAD_ARGS *arguments;
+    int *arguments;
     int i;
 
-    arguments = (THREAD_ARGS *)arg; // proper casting before dereferencing (could also be done in one statement)
-    i = (*arguments).i;             // get the integer value of the pointer
-    //free(arg);                      // we retrieved the integer value, so now the pointer can be deleted
+    arguments = (int *)arg; // proper casting before dereferencing (could also be done in one statement)
+    i = *arguments;         // get the integer value of the pointer
+    free(arg);              // we retrieved the integer value, so now the pointer can be deleted
 
     //printf("        %lx: thread started; parameter=%d\n", pthread_self(), j);
-    // for (int i = 1; i * j <= NROF_PIECES; i++)
-    // {
+    for (int j = 1; i * j <= NROF_PIECES; j++)
+    {
+        if (BIT_IS_SET(buffer[i * j / 128], i * j % 128))
+            // flip it to 0
+            BIT_CLEAR(buffer[i * j / 128], i * j % 128);
+        else
+            // If bit at position n is not set, flip to 1
+            BIT_SET(buffer[i * j / 128], i * j % 128);
+    }
 
-    //     if (BIT_IS_SET(buffer[i * j / 128], i * j % 128))
-    //     {
-    //         // flip it to 0
-    //         BIT_CLEAR(buffer[i * j / 128], i * j % 128);
-    //     }
-    //     else
-    //     {
-    //         // If bit at position n is not set, flip to 1
-    //         BIT_SET(buffer[i * j / 128], i * j % 128);
-    //     }
-    // }
-    buffer[i / 128] = buffer[i / 128] ^ BITMASK(i % 128);
-    //sem_post(&sem);
-    return (0); // you can also use pthread_exit(rtnval);
+    //int id = (*arguments).id;
+    //free_id(id);
+    sem_post(&sem);
+    pthread_detach(pthread_self());
+    pthread_exit(NULL);
 }
 
-int min(int a, int b)
-{
-    return a < b ? a : b;
-}
+// int min(int a, int b)
+// {
+//     return a < b ? a : b;
+// }
+
+// //fifo free id
+// void free_id(int id)
+// {
+//     for (int i = 0; i < NROF_THREADS; i++)
+//         if (free_ids[i] == -1)
+//         {
+//             free_ids[i] = id;
+//             return;
+//         }
+// }
+
+// //fifo take id
+// int take_id()
+// {
+//     int id;
+//     if (free_ids[0] == -1)
+//         return -1;
+//     else
+//         id = free_ids[0];
+
+//     for (int i = 1; i < NROF_THREADS; i++)
+//     {
+//         if (free_ids[i] == -1)
+//             break;
+//         free_ids[i - 1] = free_ids[i];
+//     }
+
+//     free_ids[NROF_THREADS - 1] = -1;
+
+//     return id;
+// }
